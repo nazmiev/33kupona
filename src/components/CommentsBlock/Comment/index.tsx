1867ae1299a8203bc9cee5343eb74466c58de9dd
Рома <57133971+nazmiev@users.noreply.github.com>
2023-09-01 17:22:52 +0700

    feat(comment) : comments styles improvements

--- src/components/CommentsBlock/Comment/index.tsx
+++ src/components/CommentsBlock/Comment/index.tsx
@@ -1,13 +1,26 @@
 import styles from "./Comment.module.scss";
 
-export default function Comment({comment, depth}: any) {
+export default function Comment({ comment, depth }: any) {
   return (
-    <div className={styles.comment}>[{depth}] {comment.user_name} @{comment.user_id}
-      {comment.published && (<p>{comment.html}</p>)}
-      {comment.comments.length && (
-      <div className={styles.children}>
-        {comment.comments.map((comment:any) => <Comment key={comment.id} comment={comment} depth={1 + depth} />)}
+    <div className={styles.comment}>
+      <div className={styles.header}>
+        {comment.user_avatar && (
+          <img src={`https://dn.33kupona.ru/img/avatar/small/${comment.user_avatar}`} alt={`аватарка пользователя ${comment.user_name}`}/>
+        )}
+        <div className={styles.info}>
+          <p><b>{comment.user_name}</b> @{comment.user_id} [{depth}] </p>
+          <p> 
+            {new Date(comment.date_created*1000).toLocaleDateString("ru-RU")}<> </>
+            {new Date(comment.date_created*1000).toLocaleTimeString("ru-RU")}
+          </p>
+        </div>
       </div>
+
+      {comment.published&&(<p>{comment.html}</p>)}
+      {comment.comments.length&&(
+        <div className={styles.children}>
+          {comment.comments.map((comment: any) => <Comment key={comment.id} comment={comment} depth={1 + depth} />)}
+        </div>
       )}
     </div>
   );
