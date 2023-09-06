f77ac0e5173896dde9c4be116839d91fc120a351
Роман Назмиев <roma.nazmiev@gmail.com>
2023-09-06 12:22:58 +0700

    style(comments): add gap

--- src/components/CommentsBlock/Comment/index.tsx
+++ src/components/CommentsBlock/Comment/index.tsx
@@ -20,11 +20,11 @@ export default function Comment({ comment, depth }: any) {
       </div>
 
       {comment.published&&(<p>{comment.html}</p>)}
-      {comment.comments.length&&(
+      {comment.comments.length ? (
         <div className={styles.children}>
           {comment.comments.map((comment: any) => <Comment key={comment.id} comment={comment} depth={1 + depth} />)}
         </div>
-      )}
+      ) : <></>}
     </div>
   );
 }
